e066b0512c29e37a77dfe22085e15f8bccd4e9db
Eberhard Wolff <eberhard.wolff@gmail.com>
2019-03-13 21:38:19 +0100

    Add script to fix Docker images in Kubernetes config for Google Cloud

---
+++ microservice-istio-demo/fix-microservices-gpc.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sed -i "s/image: /image: gcr.io\/$PROJECT_ID\//" microservices.yaml
+sed -i "s/image: /image: gcr.io\/$PROJECT_ID\//" infrastructure.yaml
+sed -i "s/imagePullPolicy: Never/imagePullPolicy: IfNotPresent/" infrastructure.yaml
+sed -i "s/imagePullPolicy: Never/imagePullPolicy: IfNotPresent/" microservices.yaml
