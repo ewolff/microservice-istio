935b3c9b863369cae88c45e99ab576a822998d61
Eberhard Wolff <eberhard.wolff@gmail.com>
2019-03-19 18:44:17 +0100

    Rename GCP

--- microservice-istio-demo/fix-microservices-gcp.sh
+++ microservice-istio-demo/fix-microservices-gcp.sh
@@ -3,3 +3,4 @@ sed -i "s,image: ,image: gcr.io/$PROJECT_ID/," microservices.yaml
 sed -i "s,image: ,image: gcr.io/$PROJECT_ID/," infrastructure.yaml
 sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," infrastructure.yaml
 sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," microservices.yaml
+sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," failing-order-service.yaml
