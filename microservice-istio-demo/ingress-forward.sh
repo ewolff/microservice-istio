aa0a8385e42d325865db1e99ad491d60c22cb75c
Eberhard Wolff <eberhard.wolff@gmail.com>
2019-03-28 10:30:53 +0100

    fix: ingress forward actually works

--- microservice-istio-demo/ingress-forward.sh
+++ microservice-istio-demo/ingress-forward.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo Open Ingress at http://localhost:31380/
-kubectl -n istio-system port-forward deployment/istio-ingressgateway 31380:31380
+kubectl -n istio-system port-forward deployment/istio-ingressgateway 31380:80
 
