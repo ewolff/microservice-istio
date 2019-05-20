81ec60141e407d3c757f746326bea7d5fc2b7adb
Eberhard Wolff <eberhard.wolff@gmail.com>
2019-05-20 22:51:01 +0200

    Remove deprecated Spring Data Rest configuration

--- microservice-istio-demo/microservice-istio-order/src/main/java/com/ewolff/microservice/order/logic/SpringRestDataConfig.java
+++ microservice-istio-demo/microservice-istio-order/src/main/java/com/ewolff/microservice/order/logic/SpringRestDataConfig.java
@@ -1,28 +1,20 @@
 package com.ewolff.microservice.order.logic;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
-import org.springframework.http.MediaType;
-
 import com.ewolff.microservice.order.customer.Customer;
 import com.ewolff.microservice.order.item.Item;
 
-@Configuration
-class SpringRestDataConfig extends RepositoryRestConfigurerAdapter {
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 
-	@Bean
-	public RepositoryRestConfigurer repositoryRestConfigurer() {
+@Component
+class SpringRestDataConfig implements RepositoryRestConfigurer {
 
-		return new RepositoryRestConfigurerAdapter() {
-			@Override
-			public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-				config.exposeIdsFor(Order.class, Item.class, Customer.class);
-				config.setDefaultMediaType(MediaType.APPLICATION_JSON);
-			}
-		};
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Order.class, Item.class, Customer.class);
+		config.setDefaultMediaType(MediaType.APPLICATION_JSON);
 	}
 
 }
