815d0acdc527877573e8cf5c0308dae98c2eabfd
Eberhard Wolff <eberhard.wolff@gmail.com>
2019-05-20 23:52:17 +0200

    Remove unneeded import

--- microservice-istio-demo/microservice-istio-order/src/main/java/com/ewolff/microservice/order/logic/Order.java
+++ microservice-istio-demo/microservice-istio-order/src/main/java/com/ewolff/microservice/order/logic/Order.java
@@ -11,7 +11,6 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
