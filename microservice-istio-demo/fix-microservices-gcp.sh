1d41cadc839eecc05516223241995985b379f4d5
Eberhard Wolff <eberhard.wolff@gmail.com>
2019-06-13 17:59:14 +0200

    Fix image for failing order service, too

--- microservice-istio-demo/fix-microservices-gcp.sh
+++ microservice-istio-demo/fix-microservices-gcp.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sed -i "s,image: ,image: gcr.io/$PROJECT_ID/," microservices.yaml
 sed -i "s,image: ,image: gcr.io/$PROJECT_ID/," infrastructure.yaml
+sed -i "s,image: ,image: gcr.io/$PROJECT_ID/," failing-order-service.yaml
 sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," infrastructure.yaml
 sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," microservices.yaml
 sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," failing-order-service.yaml
