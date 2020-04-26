cbe7789848ba1df5408985157406e7ac9ccdf0d0
Eberhard Wolff <eberhard.wolff@gmail.com>
2020-04-26 09:54:08 +0200

    Write Dockerhub config in different file

--- microservice-istio-demo/fix-microservices-dockerhub.sh
+++ microservice-istio-demo/fix-microservices-dockerhub.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sed -i "s,image: ,image: docker.io/ewolff/," microservices.yaml
-sed -i "s,image: ,image: docker.io/ewolff/," infrastructure.yaml
-sed -i "s,image: ,image: docker.io/ewolff/," failing-order-service.yaml
-sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," infrastructure.yaml
-sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," microservices.yaml
-sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," failing-order-service.yaml
+sed "s,image: ,image: docker.io/ewolff/," microservices.yaml >microservices-dockerhub.yaml
+sed "s,image: ,image: docker.io/ewolff/," infrastructure.yaml >infrastructure-dockerhub.yaml
+sed "s,image: ,image: docker.io/ewolff/," failing-order-service.yaml >failing-order-service-dockerhub.yaml
+sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," infrastructure-dockerhub.yaml
+sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," microservices-dockerhub.yaml
+sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," failing-order-service-dockerhub.yaml
