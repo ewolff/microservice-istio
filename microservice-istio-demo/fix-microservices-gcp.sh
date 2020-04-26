5e6dd71193e350e77cfc333bc810e685d2103fd0
Eberhard Wolff <eberhard.wolff@gmail.com>
2020-04-26 09:58:18 +0200

    Add postfix for gcp config instead of overwriting

--- microservice-istio-demo/fix-microservices-gcp.sh
+++ microservice-istio-demo/fix-microservices-gcp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sed -i "s,image: ,image: gcr.io/$PROJECT_ID/," microservices.yaml
-sed -i "s,image: ,image: gcr.io/$PROJECT_ID/," infrastructure.yaml
-sed -i "s,image: ,image: gcr.io/$PROJECT_ID/," failing-order-service.yaml
-sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," infrastructure.yaml
-sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," microservices.yaml
-sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," failing-order-service.yaml
+sed "s,image: ,image: gcr.io/$PROJECT_ID/," microservices.yaml > microservices-gcp.yaml
+sed "s,image: ,image: gcr.io/$PROJECT_ID/," infrastructure.yaml > infrastructure-gcp.yaml 
+sed "s,image: ,image: gcr.io/$PROJECT_ID/," failing-order-service.yaml > failing-order-service-gcp.yaml
+sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," infrastructure-gcp.yaml
+sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," microservices-gcp.yaml
+sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," failing-order-service-gcp.yaml
