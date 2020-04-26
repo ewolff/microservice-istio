8358a147eff74d95cb83988b3750b31f66350869
Eberhard Wolff <eberhard.wolff@gmail.com>
2020-04-26 09:47:19 +0200

    Fix URLs for Docker images

--- microservice-istio-demo/fix-microservices-dockerhub.sh
+++ microservice-istio-demo/fix-microservices-dockerhub.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sed -i "s,image: ,image: docker.pkg.github.com/ewolff/microservice-istio/," microservices.yaml
-sed -i "s,image: ,image: docker.pkg.github.com/ewolff/microservice-istio/," infrastructure.yaml
-sed -i "s,image: ,image: docker.pkg.github.com/ewolff/microservice-istio/," failing-order-service.yaml
+sed -i "s,image: ,image: docker.io/ewolff/," microservices.yaml
+sed -i "s,image: ,image: docker.io/ewolff/," infrastructure.yaml
+sed -i "s,image: ,image: docker.io/ewolff/," failing-order-service.yaml
 sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," infrastructure.yaml
 sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," microservices.yaml
 sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," failing-order-service.yaml
