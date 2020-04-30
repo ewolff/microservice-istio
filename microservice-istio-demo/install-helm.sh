336618008330cafef9b82017dd3b05a29b5fffd4
Hanna <hanna.prinz@innoq.com>
2020-04-30 20:29:16 +0200

    Helm 3 in script

--- microservice-istio-demo/install-helm.sh
+++ microservice-istio-demo/install-helm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-helm install --set name=order ../spring-boot-microservice/
-helm install --set name=invoicing ../spring-boot-microservice/
-helm install --set name=shipping ../spring-boot-microservice/
+helm install order --set name=order ../spring-boot-microservice/
+helm install invoicing --set name=invoicing ../spring-boot-microservice/
+helm install shipping --set name=shipping ../spring-boot-microservice/
