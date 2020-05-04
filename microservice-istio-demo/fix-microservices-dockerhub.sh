04ac7e3e67285d206fbf519e3e1329b1a5b24a00
Hanna <hanna.prinz@innoq.com>
2020-05-04 12:10:16 +0200

    changes to scripts for zsh (should work on any machine, but is worth checking)

--- microservice-istio-demo/fix-microservices-dockerhub.sh
+++ microservice-istio-demo/fix-microservices-dockerhub.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sed "s,image: ,image: docker.io/ewolff/," microservices.yaml >microservices-dockerhub.yaml
-sed "s,image: ,image: docker.io/ewolff/," infrastructure.yaml >infrastructure-dockerhub.yaml
-sed "s,image: ,image: docker.io/ewolff/," failing-order-service.yaml >failing-order-service-dockerhub.yaml
-sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," infrastructure-dockerhub.yaml
-sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," microservices-dockerhub.yaml
-sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," failing-order-service-dockerhub.yaml
+sed "s/image: /image: docker.io\/ewolff\//g" microservices.yaml > microservices-dockerhub.yaml
+sed "s/image: /image: docker.io\/ewolff\//g" infrastructure.yaml > infrastructure-dockerhub.yaml
+sed "s/image: /image: docker.io\/ewolff\//g" failing-order-service.yaml > failing-order-service-dockerhub.yaml
+sed -i "" "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," infrastructure-dockerhub.yaml
+sed -i "" "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," microservices-dockerhub.yaml
+sed -i "" "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," failing-order-service-dockerhub.yaml
--- microservice-istio-demo/fix-microservices-gcp.sh
+++ microservice-istio-demo/fix-microservices-gcp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sed "s,image: ,image: gcr.io/$PROJECT_ID/," microservices.yaml > microservices-gcp.yaml
-sed "s,image: ,image: gcr.io/$PROJECT_ID/," infrastructure.yaml > infrastructure-gcp.yaml 
-sed "s,image: ,image: gcr.io/$PROJECT_ID/," failing-order-service.yaml > failing-order-service-gcp.yaml
-sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," infrastructure-gcp.yaml
-sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," microservices-gcp.yaml
-sed -i "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," failing-order-service-gcp.yaml
+sed "s/image: /image: gcr.io\/$PROJECT_ID\//g" microservices.yaml > microservices-gcp.yaml
+sed "s/image: /image: gcr.io\/$PROJECT_ID\//g" infrastructure.yaml > infrastructure-gcp.yaml 
+sed "s/image: /image: gcr.io\/$PROJECT_ID\//g" failing-order-service.yaml > failing-order-service-gcp.yaml
+sed -i "" "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," infrastructure-gcp.yaml
+sed -i "" "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," microservices-gcp.yaml
+sed -i "" "s,imagePullPolicy: Never,imagePullPolicy: IfNotPresent," failing-order-service-gcp.yaml
