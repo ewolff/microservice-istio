ff47e46540c787ae6807ca14ece536baaf98d358
Eberhard Wolff <eberhard.wolff@gmail.com>
2021-03-21 17:44:53 +0100

    Update logstash encoder

--- microservice-istio-demo/microservice-istio-order/src/main/java/com/ewolff/microservice/order/OrderFeed.java
+++ microservice-istio-demo/microservice-istio-order/src/main/java/com/ewolff/microservice/order/OrderFeed.java
@@ -5,9 +5,8 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
-
-import net.logstash.logback.encoder.org.apache.commons.lang.builder.HashCodeBuilder;
 
 public class OrderFeed {
 
