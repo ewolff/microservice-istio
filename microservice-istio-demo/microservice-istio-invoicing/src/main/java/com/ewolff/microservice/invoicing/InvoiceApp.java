11de82a8ea0d9c52b0affb7203c42daf352d4152
Eberhard Wolff <eberhard.wolff@gmail.com>
2021-03-28 13:50:07 +0200

    Invoicing microservice now also native

--- microservice-istio-demo/microservice-istio-invoicing/src/main/java/com/ewolff/microservice/invoicing/InvoiceApp.java
+++ microservice-istio-demo/microservice-istio-invoicing/src/main/java/com/ewolff/microservice/invoicing/InvoiceApp.java
@@ -2,10 +2,21 @@ package com.ewolff.microservice.invoicing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.AccessBits;
+import org.springframework.nativex.hint.ProxyHint;
+import org.springframework.nativex.hint.TypeHint;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.thymeleaf.engine.IterationStatusVar;
+
+import com.ewolff.microservice.invoicing.poller.OrderFeed;
+import com.ewolff.microservice.invoicing.poller.OrderFeedEntry;
 
 @EnableScheduling
 @SpringBootApplication
+@TypeHint(types = { Customer.class, Item.class, Invoice.class, Address.class, InvoiceLine.class, OrderFeed.class,
+		OrderFeedEntry.class }, access = AccessBits.ALL)
+@TypeHint(types = { IterationStatusVar.class }, access = AccessBits.ALL)
+@ProxyHint(types = InvoiceService.class)
 public class InvoiceApp {
 
 	public static void main(String[] args) {
@@ -13,3 +24,4 @@ public class InvoiceApp {
 	}
 
 }
+	
---
+++ microservice-istio-demo/microservice-istio-invoicing/src/main/java/com/ewolff/microservice/invoicing/InvoiceServiceImpl.java
@@ -0,0 +1,32 @@
+package com.ewolff.microservice.invoicing;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class InvoiceServiceImpl implements InvoiceService {
+
+	private final Logger log = LoggerFactory.getLogger(InvoiceServiceImpl.class);
+
+	private InvoiceRepository invoiceRepository;
+
+	@Autowired
+	public InvoiceServiceImpl(InvoiceRepository invoiceRepository) {
+		super();
+		this.invoiceRepository = invoiceRepository;
+	}
+
+	@Override
+	@Transactional
+	public void generateInvoice(Invoice invoice) {
+		if (invoiceRepository.existsById(invoice.getId())) {
+			log.info("Invoice id {} already exists - ignored", invoice.getId());
+		} else {
+			invoiceRepository.save(invoice);
+		}
+	}
+
+}
