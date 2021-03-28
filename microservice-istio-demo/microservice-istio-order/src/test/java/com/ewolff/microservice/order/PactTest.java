b8757eab46ce4c95fac985ab120890a8f7549530
Eberhard Wolff <eberhard.wolff@gmail.com>
2021-03-28 18:33:05 +0200

    Update Pact version

--- microservice-istio-demo/microservice-istio-order/src/test/java/com/ewolff/microservice/order/PactTest.java
+++ microservice-istio-demo/microservice-istio-order/src/test/java/com/ewolff/microservice/order/PactTest.java
@@ -8,11 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import au.com.dius.pact.provider.junit.Provider;
-import au.com.dius.pact.provider.junit.loader.PactFolder;
 import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = OrderApp.class, webEnvironment = WebEnvironment.DEFINED_PORT)
