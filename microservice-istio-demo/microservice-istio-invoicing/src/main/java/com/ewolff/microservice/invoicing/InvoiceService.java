272365764572386ebd70b64f994753688da8a5dd
Eberhard Wolff <eberhard.wolff@gmail.com>
2021-03-28 14:17:34 +0200

    Remove unneeded import

--- microservice-istio-demo/microservice-istio-invoicing/src/main/java/com/ewolff/microservice/invoicing/InvoiceService.java
+++ microservice-istio-demo/microservice-istio-invoicing/src/main/java/com/ewolff/microservice/invoicing/InvoiceService.java
@@ -1,7 +1,5 @@
 package com.ewolff.microservice.invoicing;
 
-import org.springframework.transaction.annotation.Transactional;
-
 public interface InvoiceService {
 
 	void generateInvoice(Invoice invoice);
