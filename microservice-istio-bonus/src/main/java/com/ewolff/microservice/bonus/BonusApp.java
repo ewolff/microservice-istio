287185e3ccf12b8871ff0293f12865494101373f
Eberhard Wolff <eberhard.wolff@gmail.com>
2021-03-30 09:47:23 +0200

    Add code to enable native bonus microservice

--- microservice-istio-bonus/src/main/java/com/ewolff/microservice/bonus/BonusApp.java
+++ microservice-istio-bonus/src/main/java/com/ewolff/microservice/bonus/BonusApp.java
@@ -2,10 +2,20 @@ package com.ewolff.microservice.bonus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.AccessBits;
+import org.springframework.nativex.hint.ProxyHint;
+import org.springframework.nativex.hint.TypeHint;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.thymeleaf.engine.IterationStatusVar;
+
+import com.ewolff.microservice.bonus.poller.OrderFeed;
+import com.ewolff.microservice.bonus.poller.OrderFeedEntry;
 
 @EnableScheduling
 @SpringBootApplication
+@TypeHint(types = { Customer.class, Bonus.class, OrderFeed.class, OrderFeedEntry.class }, access = AccessBits.ALL)
+@TypeHint(types = { IterationStatusVar.class }, access = AccessBits.ALL)
+@ProxyHint(types = BonusService.class)
 public class BonusApp {
 
 	public static void main(String[] args) {
---
+++ microservice-istio-bonus/src/main/java/com/ewolff/microservice/bonus/BonusServiceImpl.java
@@ -0,0 +1,32 @@
+package com.ewolff.microservice.bonus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class BonusServiceImpl implements BonusService {
+
+	private final Logger log = LoggerFactory.getLogger(BonusServiceImpl.class);
+
+	private BonusRepository bonusRepository;
+
+	@Autowired
+	public BonusServiceImpl(BonusRepository bonusRepository) {
+		super();
+		this.bonusRepository = bonusRepository;
+	}
+
+	@Override
+	@Transactional
+	public void calculateBonus(Bonus bonus) {
+		if (bonusRepository.existsById(bonus.getId())) {
+			log.info("Bonus id {} already exists - ignored", bonus.getId());
+		} else {
+			bonusRepository.save(bonus);
+		}
+	}
+
+}
