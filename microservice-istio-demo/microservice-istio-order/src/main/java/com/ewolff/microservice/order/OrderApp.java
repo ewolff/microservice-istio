8085d760999b5e68439ab40e83567cea29619511
Eberhard Wolff <eberhard.wolff@gmail.com>
2021-11-02 09:29:34 +0100

    Remove now unneeded TypeHint

--- microservice-istio-demo/microservice-istio-order/src/main/java/com/ewolff/microservice/order/OrderApp.java
+++ microservice-istio-demo/microservice-istio-order/src/main/java/com/ewolff/microservice/order/OrderApp.java
@@ -2,12 +2,8 @@ package com.ewolff.microservice.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.AccessBits;
-import org.springframework.nativex.hint.TypeHint;
-import org.thymeleaf.engine.IterationStatusVar;
 
 @SpringBootApplication
-@TypeHint(types = { IterationStatusVar.class }, access = AccessBits.ALL)
 public class OrderApp {
 
 	public static void main(String[] args) {
