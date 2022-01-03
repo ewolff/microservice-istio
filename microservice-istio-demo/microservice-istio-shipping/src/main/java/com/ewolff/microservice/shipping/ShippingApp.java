abf50a2f64da29d0b7f2db7885332bb41497d829
Eberhard Wolff <eberhard.wolff@gmail.com>
2022-01-03 17:20:35 +0100

    Update Annotations to Spring Native 0.11.1

--- microservice-istio-demo/microservice-istio-shipping/src/main/java/com/ewolff/microservice/shipping/ShippingApp.java
+++ microservice-istio-demo/microservice-istio-shipping/src/main/java/com/ewolff/microservice/shipping/ShippingApp.java
@@ -2,7 +2,6 @@ package com.ewolff.microservice.shipping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.AccessBits;
 import org.springframework.nativex.hint.TypeHint;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.thymeleaf.engine.IterationStatusVar;
@@ -12,8 +11,8 @@ import com.ewolff.microservice.shipping.poller.OrderFeedEntry;
 
 @EnableScheduling
 @SpringBootApplication
-@TypeHint(types = { OrderFeed.class, OrderFeedEntry.class }, access = AccessBits.ALL)
-@TypeHint(types = { IterationStatusVar.class }, access = AccessBits.ALL)
+@TypeHint(types = { OrderFeed.class, OrderFeedEntry.class })
+@TypeHint(types = { IterationStatusVar.class })
 public class ShippingApp {
 
 	public static void main(String[] args) {
