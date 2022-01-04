40285dac25bdf2492b54be1bef9f2a4322de9654
Eberhard Wolff <eberhard.wolff@gmail.com>
2022-01-04 09:09:57 +0100

    Update Bonus microservice to Spring Native 0.11.1 / Spring Boot 2.6.2 / Spring Cloud 2021.0.0

--- microservice-istio-bonus/docker-build-native.sh
+++ microservice-istio-bonus/docker-build-native.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn clean spring-boot:build-image
+mvn clean spring-boot:build-image --file pom-native.xml
 docker tag microservice-istio-bonus:0.0.1-SNAPSHOT microservice-istio-bonus-native:1
--- microservice-istio-bonus/src/main/java/com/ewolff/microservice/bonus/BonusApp.java
+++ microservice-istio-bonus/src/main/java/com/ewolff/microservice/bonus/BonusApp.java
@@ -1,19 +1,22 @@
 package com.ewolff.microservice.bonus;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.AccessBits;
+import org.springframework.nativex.hint.TypeAccess;
 import org.springframework.nativex.hint.TypeHint;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.thymeleaf.engine.IterationStatusVar;
 
 import com.ewolff.microservice.bonus.poller.OrderFeed;
 import com.ewolff.microservice.bonus.poller.OrderFeedEntry;
 
 @EnableScheduling
 @SpringBootApplication
-@TypeHint(types = { OrderFeed.class, OrderFeedEntry.class }, access = AccessBits.ALL)
-@TypeHint(types = { IterationStatusVar.class }, access = AccessBits.ALL)
+@TypeHint(types = { OrderFeed.class, OrderFeedEntry.class, ArrayList.class }, access = { TypeAccess.PUBLIC_CONSTRUCTORS,
+		TypeAccess.DECLARED_CONSTRUCTORS, TypeAccess.DECLARED_FIELDS, TypeAccess.DECLARED_CLASSES,
+		TypeAccess.DECLARED_METHODS, TypeAccess.PUBLIC_FIELDS, TypeAccess.PUBLIC_METHODS,
+		TypeAccess.QUERY_DECLARED_CONSTRUCTORS, TypeAccess.QUERY_PUBLIC_METHODS })
 public class BonusApp {
 
 	public static void main(String[] args) {
